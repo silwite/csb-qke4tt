c9f8984789c19579c56d225fdfb8d3212e75d7fa
Code Sandbox <hello@codesandbox.io>
2023-01-04 05:31:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6ffff538ad3ab7dc01f17455e894adc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4409f5f968a472690dbcf9b646bbbee",
+    "url": "https://silwite.github.io/csb-qke4tt/index.html"
+  },
+  {
+    "revision": "e43d5ed081daab49ce67",
+    "url": "https://silwite.github.io/csb-qke4tt/static/js/2.4fc658ef.chunk.js"
+  },
+  {
+    "revision": "20b44e3e12e8a7109c0d",
+    "url": "https://silwite.github.io/csb-qke4tt/static/js/main.3b9a3e7b.chunk.js"
+  },
+  {
+    "revision": "b5c67084076ac14326a4",
+    "url": "https://silwite.github.io/csb-qke4tt/static/js/runtime-main.98038f87.js"
+  }
+]);
